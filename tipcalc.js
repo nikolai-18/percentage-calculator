d30b408cb48a89126813b1dfe44ae20e9cb9b059
nikolai-18 <59432018+nikolai-18@users.noreply.github.com>
2020-01-02 14:18:00 +0500

    Add files via upload

---
+++ tipcalc.js
@@ -0,0 +1,12 @@
+function calculate (){
+    let amount = $('#amount').val();
+    let percentage = $('#percentage').val();
+    let tip = amount * (percentage /100);
+    let total = Number(amount) + tip;
+
+    $('#tip').val( tip.toFixed(2));
+    $('#total').val( total.toFixed(2));
+
+    return false;
+}
+$('#calculator').submit(calculate);
